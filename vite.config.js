9f7c3b1f72d767547384c2b014555a09dbe94043
Hideo Kasai <hiitan.chiichii@gmail.com>
2025-07-08 01:07:08 +0900

    modified all issue

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: process.env.NODE_ENV === 'production' 
+        ? (process.env.VITE_APP_URL || 'https://microposts-hkprj-home.fly.dev') + '/'
+        : '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
